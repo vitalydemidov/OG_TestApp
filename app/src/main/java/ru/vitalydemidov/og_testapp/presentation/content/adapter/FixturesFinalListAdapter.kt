e0a10b4e726f133c921c2fdd86769b58938babdd
vitalydemidov <mr.vitalydemidov@gmail.com>
2019-02-21 23:42:59 +0300

    Renamed fixtures adapters classes for more clarify

--- app/src/main/java/ru/vitalydemidov/og_testapp/presentation/content/adapter/FixturesFinalListAdapter.kt
+++ app/src/main/java/ru/vitalydemidov/og_testapp/presentation/content/adapter/FixturesFinalListAdapter.kt
@@ -1,6 +1,5 @@
-package ru.vitalydemidov.og_testapp.presentation.content
+package ru.vitalydemidov.og_testapp.presentation.content.adapter
 
-import android.support.annotation.UiThread
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import ru.vitalydemidov.og_testapp.R
@@ -9,23 +8,19 @@ import ru.vitalydemidov.og_testapp.appcommon.adapter.BaseDelegateAdapter
 import ru.vitalydemidov.og_testapp.appcommon.adapter.BaseViewTypeDelegate
 import ru.vitalydemidov.og_testapp.appcommon.model.BaseItem
 import ru.vitalydemidov.og_testapp.presentation.content.viewholder.DateDividerViewHolder
-import ru.vitalydemidov.og_testapp.presentation.content.viewholder.FixtureUpcomingViewHolder
+import ru.vitalydemidov.og_testapp.presentation.content.viewholder.FixtureResultViewHolder
 import ru.vitalydemidov.og_testapp.presentation.content.viewmodel.DateDividerVM
-import ru.vitalydemidov.og_testapp.presentation.content.viewmodel.FixtureUpcomingVM
+import ru.vitalydemidov.og_testapp.presentation.content.viewmodel.FixtureResultVM
 
-@UiThread
-class FixturesListAdapter : BaseDelegateAdapter<BaseItem<in Nothing>>() {
+class FixturesFinalListAdapter : BaseDelegateAdapter<BaseItem<in Nothing>>() {
 
     init {
         addViewTypeDelegate(
-            R.id.fixture_upcoming_item_id,
-            object : BaseViewTypeDelegate<FixtureUpcomingVM, AbstractBaseItemViewHolder<FixtureUpcomingVM>>() {
+            R.id.fixture_final_item_id,
+            object : BaseViewTypeDelegate<FixtureResultVM, AbstractBaseItemViewHolder<FixtureResultVM>>() {
 
-                override fun onCreateViewHolder(
-                    parent: ViewGroup,
-                    viewType: Int
-                ): AbstractBaseItemViewHolder<FixtureUpcomingVM> =
-                    FixtureUpcomingViewHolder(
+                override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AbstractBaseItemViewHolder<FixtureResultVM> =
+                    FixtureResultViewHolder(
                         LayoutInflater.from(parent.context).inflate(R.layout.item_list_fixture_upcoming, parent, false)
                     )
             }
--- app/src/main/java/ru/vitalydemidov/og_testapp/presentation/content/adapter/FixturesUpcomingListAdapter.kt
+++ app/src/main/java/ru/vitalydemidov/og_testapp/presentation/content/adapter/FixturesUpcomingListAdapter.kt
@@ -1,5 +1,6 @@
-package ru.vitalydemidov.og_testapp.presentation.content
+package ru.vitalydemidov.og_testapp.presentation.content.adapter
 
+import android.support.annotation.UiThread
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import ru.vitalydemidov.og_testapp.R
@@ -8,19 +9,23 @@ import ru.vitalydemidov.og_testapp.appcommon.adapter.BaseDelegateAdapter
 import ru.vitalydemidov.og_testapp.appcommon.adapter.BaseViewTypeDelegate
 import ru.vitalydemidov.og_testapp.appcommon.model.BaseItem
 import ru.vitalydemidov.og_testapp.presentation.content.viewholder.DateDividerViewHolder
-import ru.vitalydemidov.og_testapp.presentation.content.viewholder.FixtureResultViewHolder
+import ru.vitalydemidov.og_testapp.presentation.content.viewholder.FixtureUpcomingViewHolder
 import ru.vitalydemidov.og_testapp.presentation.content.viewmodel.DateDividerVM
-import ru.vitalydemidov.og_testapp.presentation.content.viewmodel.FixtureResultVM
+import ru.vitalydemidov.og_testapp.presentation.content.viewmodel.FixtureUpcomingVM
 
-class ResultsListAdapter : BaseDelegateAdapter<BaseItem<in Nothing>>() {
+@UiThread
+class FixturesUpcomingListAdapter : BaseDelegateAdapter<BaseItem<in Nothing>>() {
 
     init {
         addViewTypeDelegate(
-            R.id.fixture_final_item_id,
-            object : BaseViewTypeDelegate<FixtureResultVM, AbstractBaseItemViewHolder<FixtureResultVM>>() {
+            R.id.fixture_upcoming_item_id,
+            object : BaseViewTypeDelegate<FixtureUpcomingVM, AbstractBaseItemViewHolder<FixtureUpcomingVM>>() {
 
-                override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AbstractBaseItemViewHolder<FixtureResultVM> =
-                    FixtureResultViewHolder(
+                override fun onCreateViewHolder(
+                    parent: ViewGroup,
+                    viewType: Int
+                ): AbstractBaseItemViewHolder<FixtureUpcomingVM> =
+                    FixtureUpcomingViewHolder(
                         LayoutInflater.from(parent.context).inflate(R.layout.item_list_fixture_upcoming, parent, false)
                     )
             }
